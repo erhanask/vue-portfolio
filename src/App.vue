23cf0f4ea1685fa1304ba1677dbad9b4aea1eee3
Erhan <77926871+erhanask@users.noreply.github.com>
2022-03-08 12:53:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello Vju in CodeSandbox!" />
+
+    <Trying />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Trying from "./components/Trying";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Trying,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Trying.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="colorized-red">Aşk sevgi çiçek kavga.</h1>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Trying",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 20px 0 0;
+  padding: 1rem;
+}
+
+.colorized-red {
+  color: white;
+  background-color: rgb(240, 0, 0);
+}
+</style>
